68f2ba9a6f37380a121a877183e3c4ba99f7de77
Daniel Hughes <danielhughes23@hotmail.com>
2017-11-01 15:54:31 +0000

    Making progress with the final exercise.

--- request.php
+++ request.php
@@ -1,8 +1,59 @@
 <?php
-
-/*
 // accept a term (keyword)
 // respond with a value
+
+//$query = $_GET['q'];
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <definitiona>
+        <name>definition</name>
+        <p>A statement of the exact meaning of a word, especially in a dictionary.</p>
+        <author>-John</author>
+    </definitiona>
+    
+    <definitiona>
+        <name>bar</name>
+        <p>A place that sells alcholic beverages.</p>
+        <author>-Mary</author>
+    </definitiona>
+    
+    <definitiona>
+        <name>ajax</name>
+        <p>Technique which involves the use of javascript and xml (or JSON).</p>
+        <author>-Kimberly</author>
+    </definitiona>
+    
+    <definitiona>
+        <name>html</name>
+        <p>The standard markup language for creating web pages and web applications.</p>
+        <author>-Marshall</author>
+    </definitiona>
+    
+    <definitiona>
+        <name>css</name>
+        <p>A style sheet language used for describing the presentation of a document written in a markup language.</p>
+        <author>-Brittani</author>
+    </definitiona>
+    
+    <definitiona>
+        <name>javascript</name>
+        <p>A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</p>
+        <author>-Simone</author>
+    </definitiona>
+    
+    <definitiona>
+        <name>php</name>
+        <p>A server-side scripting language, and a powerful tool for making dynamic and interactive websites.</p>
+        <author>-Gavin</author>
+    </definitiona>
+</entries>';
+
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML();
+/*
+print "<h3>" . strtoupper($query) . "</h3>";
+print "<p>" . $definitiona[$query] . "</p>";
 
 $query = $_GET['q'];
 $definition = [
@@ -16,54 +67,4 @@ $definition = [
 ];
 
 print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
-*/
-
-$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
-<entries>
-    <definitiona>
-        <name><h2>definition</h2></name>
-        <p>A statement of the exact meaning of a word, especially in a dictionary.</p>
-        <author>-John</author>
-    </definitiona>
-    
-    <definitiona>
-        <name><h2>bar</h2></name>
-        <p>A place that sells alcholic beverages.</p>
-        <author>-Mary</author>
-    </definitiona>
-    
-    <definitiona>
-        <name><h2>ajax</h2></name>
-        <p>Technique which involves the use of javascript and xml (or JSON).</p>
-        <author>-Kimberly</author>
-    </definitiona>
-    
-    <definitiona>
-        <name><h2>html</h2></name>
-        <p>The standard markup language for creating web pages and web applications.</p>
-        <author>-Marshall</author>
-    </definitiona>
-    
-    <definitiona>
-        <name><h2>css</h2></name>
-        <p>A style sheet language used for describing the presentation of a document written in a markup language.</p>
-        <author>-Brittani</author>
-    </definitiona>
-    
-    <definitiona>
-        <name><h2>javascript</h2></name>
-        <p>A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</p>
-        <author>-Simone</author>
-    </definitiona>
-    
-    <definitiona>
-        <name><h2>php</h2></name>
-        <p>A server-side scripting language, and a powerful tool for making dynamic and interactive websites.</p>
-        <author>-Gavin</author>
-    </definitiona>
-</entries>';
-
-header('Content-Type: text/xml');
-$xmlOutput = new SimpleXMLElement($xmldata);
-echo $xmlOutput->asXML();
+print "<p>" . $definition[$query] . "</p>";*/
